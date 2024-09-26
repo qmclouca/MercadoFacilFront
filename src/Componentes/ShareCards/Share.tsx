c7d79b1b45a02b8415f711b36ff1ded6138571d6
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2024-09-26 20:38:57 -0300

    Criando página interna

---
+++ src/Componentes/ShareCards/Share.tsx
@@ -0,0 +1,19 @@
+import './Share.css'; 
+
+
+const Share: React.FC<ShareCardProps> = ({ logoUrl, longName, symbol, regularMarketPrice}) => {
+    return (
+        <>
+           <div className="card card-extra-styles">
+                <h2 className="card-title">{longName}</h2>
+                <img src={logoUrl} className="share-card__image" alt="Share logo"/>
+                <div className="card-body">
+                    <p className="card-text">{symbol}</p>
+                    <p className="card-text">{regularMarketPrice}</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Share;
