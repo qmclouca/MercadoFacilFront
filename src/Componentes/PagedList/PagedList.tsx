5f693e4d0a16453a039d50c3dd5b8a4c883d12aa
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2024-10-16 19:32:00 -0300

    maior parte resolvida

--- src/Componentes/PagedList/PagedList.tsx
+++ src/Componentes/PagedList/PagedList.tsx
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Share from "../ShareCards/Share";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { FetchShareListPaged } from "../../Servicos/MercadoFacilAPI";
 
 const PagedList: React.FC = () => {
+    const [shares, setShares] = useState<any[]>([]);
+    const [page, setPage] = useState(1);
+    const [resultsPerPage] = useState(6); // 6 resultados por página
+
+    useEffect(() => {
+        const fetchShares = async () => {
+            try {
+                const data = await FetchShareListPaged(page, resultsPerPage);
+                setShares(data);
+            } catch (error) {
+                console.error('Erro ao carregar ações: ', error);
+            }
+        };
+
+        fetchShares();
+    }, [page, resultsPerPage]);
+
     return (
         <>
             <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
-                    <div className="carousel-item active" data-bs-interval="10000">
-                        <div className="row">
-                            <div className="col-4 card-spacing">
-                                <Share logoUrl="https://s3-symbol-logo.tradingview.com/brasileiro-petrobras--big.svg" longName="Petróleo Brasileiro S.A. - Petrobras" symbol="PETR4" regularMarketPrice="2,000"/>
-                            </div>
-                            <div className="col-4 card-spacing">
-                                <Share logoUrl="https://s3-symbol-logo.tradingview.com/brasileiro-petrobras--big.svg" longName="Petróleo Brasileiro S.A. - Petrobras" symbol="PETR4" regularMarketPrice="2,000"/>
-                            </div>
-                            <div className="col-4 card-spacing">
-                                <Share logoUrl="https://s3-symbol-logo.tradingview.com/brasileiro-petrobras--big.svg" longName="Petróleo Brasileiro S.A. - Petrobras" symbol="PETR4" regularMarketPrice="2,000"/>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4 card-spacing">
-                                <Share logoUrl="https://s3-symbol-logo.tradingview.com/brasileiro-petrobras--big.svg" longName="Petróleo Brasileiro S.A. - Petrobras" symbol="PETR4" regularMarketPrice="2,000"/>
-                            </div>
-                            <div className="col-4 card-spacing">
-                                <Share logoUrl="https://s3-symbol-logo.tradingview.com/brasileiro-petrobras--big.svg" longName="Petróleo Brasileiro S.A. - Petrobras" symbol="PETR4" regularMarketPrice="2,000"/>
-                            </div>
-                            <div className="col-4 card-spacing">
-                                <Share logoUrl="https://s3-symbol-logo.tradingview.com/brasileiro-petrobras--big.svg" longName="Petróleo Brasileiro S.A. - Petrobras" symbol="PETR4" regularMarketPrice="2,000"/>
+                    {shares.length > 0 ? (
+                        <div className="carousel-item active" data-bs-interval="10000">
+                            <div className="row">
+                                {shares.map((share, index) => (
+                                    <div className="col-4 card-spacing" key={index}>
+                                        <Share 
+                                            logoUrl={share.logoUrl} 
+                                            longName={share.longName} 
+                                            symbol={share.symbol} 
+                                            regularMarketPrice={share.regularMarketPrice} 
+                                        />
+                                    </div>
+                                ))}
                             </div>
                         </div>
-                    </div>
-
-                    {/* Adicione mais carousel-item conforme necessário */}
+                    ) : (
+                        <p>Carregando ações...</p>
+                    )}
                 </div>
 
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
